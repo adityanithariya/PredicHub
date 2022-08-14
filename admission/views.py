c0c0ddc3723abba9c3f1646774cd24398e927a07
Aditya Soni <adityanithariya@gmail.com>
2022-08-14 22:37:33 +0530

    Prediction Form Endpoint Created

--- admission/views.py
+++ admission/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def admPredicForm(request):
+    context = {
+
+    }
+    return render(request, "admission/admissionPredicForm.html", context=context)
