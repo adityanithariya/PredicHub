6a6511b42079ebff35b55d597569a37f19f7f3a9
Aditya Soni <adityanithariya@gmail.com>
2022-08-16 23:47:39 +0530

    WIP: Student Model Initialised

--- admission/models.py
+++ admission/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .vars import State
 
-# Create your models here.
+# Models
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    state = models.CharField(max_length=20, choices=State)
+    
