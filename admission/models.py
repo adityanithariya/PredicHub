0a883521648451fa50c47cb44ac685757737f7bd
Aditya Soni <adityanithariya@gmail.com>
2022-08-18 23:15:13 +0530

    Code Refactoring and Bug Fixes in Student Model

--- admission/models.py
+++ admission/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 from .vars import State, Gender, Category1, Category2
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+# Variables
+State.insert(0, (None, "- Select -"))
+Gender.insert(0, (None, "- Select -"))
+Category1.insert(0, (None, "- Select -"))
+Category2.insert(0, (None, "- Select -"))
+
 # Utility Functions
 def studentProfilePath(instance, filename):
-    return f"admssion/student/{instance.user.username}/{filename}"
+    return f"admission/student/{instance.user.username}/{filename}"
 
 # Models
 class Student(models.Model):
@@ -20,19 +26,21 @@ class Student(models.Model):
     )
     state = models.CharField(max_length=2, choices=State)
     gender = models.CharField(max_length=1, choices=Gender)
-    category1 = models.CharField(max_length=3, choices=Category1)
-    category2 = models.CharField(max_length=3, choices=Category2)
+    category1 = models.CharField(max_length=3, choices=Category1, verbose_name="Caste")
+    category2 = models.CharField(max_length=3, choices=Category2, verbose_name="Category")
     _12th = models.FloatField(
         validators=[
             MaxValueValidator(100),
             MinValueValidator(0)
-        ]
+        ],
+        verbose_name="12th Percentage"
     )
     JEEScore = models.FloatField(
         validators=[
             MaxValueValidator(100),
             MinValueValidator(0)
-        ]
+        ],
+        verbose_name="JEE Percentile"
     )
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.user.username}"
